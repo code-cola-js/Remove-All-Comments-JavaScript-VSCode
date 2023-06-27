d3efc99f305c705550566f47653a245cbbd584a1
shizheng <ZhengShiWork@outlook.com>
2023-06-27 14:01:46 +0800

    isSupportLanguage

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,19 @@
 import * as vscode from "vscode";
-import { handler } from "./removeAllComments";
+import { isSupportLanguageCheck, removeComment } from "./removeAllComments";
 
 export function activate(context: vscode.ExtensionContext) {
 
-  const disposable = vscode.commands.registerTextEditorCommand("remove-all-comments-javascript.removeAllComments", (textEditor:vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-    handler(textEditor, edit).removeComment(); 
+  const disposable = vscode.commands.registerTextEditorCommand("remove-all-comments-javascript.removeAllComments", (textEditor:vscode.TextEditor) => {
+
+    const isSupportLanguage: boolean = isSupportLanguageCheck(textEditor);
+
+    if (!isSupportLanguage) {
+      vscode.window.showErrorMessage("This language is not supported!");
+
+      return;
+    }
+
+    removeComment(textEditor);
   });
 
   vscode.window.showInformationMessage("Remove successfully!");
--- src/removeAllComments.ts
+++ src/removeAllComments.ts
@@ -1,63 +1,71 @@
 import vscode = require("vscode");
 
-export function handler(editor: vscode.TextEditor) {
+const supportLanguage = [
+  "javascript",
+  "typescript",
+  "javascriptreact",
+  "typescriptreact",
+];
+
+const getFileInfo = (editor:vscode.TextEditor) => {
   const { document } = editor;
 
-  function getFileInfo() {
-    const selections = [
-      new vscode.Selection(new vscode.Position(0, 0), document.positionAt(document.getText().length)),
-    ];
-    const selection = selections[0];
-    const startLine = selection.start.line;
-    const endLine = selection.end.line;
-
-    return {
-      selection,
-      startLine,
-      endLine
-    };
-  }
-
-  function removeComment() {
-    const { selection, startLine, endLine } = getFileInfo();
-
-    editor
-      .edit((edit) => {
-        edit.replace(new vscode.Range(new vscode.Position(startLine, 0), new vscode.Position(endLine, selection.end.character)), document
-          .getText()
-          .replace(/\/\*(?<=\/\*)[^\/\*]{0,}?(?=\*\/)\*\//g, "")
-          .replace(/(?<=.*".*".*)\s*\/\/\s*.*|\s*(?<!".*)\/\/\s*.*/g, "")
-          .replace(/(?<!(('|"|`).*))\/\*(.*)\*\//g, "")
-          .replace(/\/\*(\s*\n.*[^\/\*])+(\*\/)$/gm, "")
-          .replace(/(\/\*\*|\/\*)(\s*|.*\n\s*\*.*)*(\/)$/gm, ""));
-      })
-      .then(formatFile);
-  }
-
-  function formatFile() {
-    const { endLine } = getFileInfo();
-
-    editor.edit((edit) => {
-      for (let line = 0; line < endLine; line++) {
-        const currentLineText = document.lineAt(line);
-
-        if (currentLineText.isEmptyOrWhitespace) {
-          edit.delete(new vscode.Selection(new vscode.Position(line, 0), new vscode.Position(line + 1, 0)));
-        }
-      }
-    });
-  }
-
-  const supportLanguage = [
-    "javascript",
-    "typescript",
-    "javascriptreact",
-    "typescriptreact",
+  const selections = [
+    new vscode.Selection(new vscode.Position(0, 0), document.positionAt(document.getText().length)),
   ];
-  const language = editor.document.languageId.toString();
+  const selection = selections[0];
+  const startLine = selection.start.line;
+  const endLine = selection.end.line;
 
   return {
-    isSupportLanguage: !!supportLanguage.find((_supportLanguage) => _supportLanguage === language),
-    removeComment,
+    selection,
+    startLine,
+    endLine
   };
-}
+};
+
+const formatFile = (editor:vscode.TextEditor) => {
+
+  const { document } = editor;
+
+  const { endLine } = getFileInfo(editor);
+
+  editor.edit((edit) => {
+    for (let line = 0; line < endLine; line++) {
+      const currentLineText = document.lineAt(line);
+
+      if (currentLineText.isEmptyOrWhitespace) {
+        edit.delete(new vscode.Selection(new vscode.Position(line, 0), new vscode.Position(line + 1, 0)));
+      }
+    }
+  });
+};
+
+export const removeComment = (editor:vscode.TextEditor) => {
+  const { selection, startLine, endLine } = getFileInfo(editor);
+
+  const { document } = editor;
+
+  editor
+    .edit((edit) => {
+      edit.replace(new vscode.Range(new vscode.Position(startLine, 0), new vscode.Position(endLine, selection.end.character)), document
+        .getText()
+        .replace(/\/\*(?<=\/\*)[^\/\*]{0,}?(?=\*\/)\*\//g, "")
+        .replace(/(?<=.*".*".*)\s*\/\/\s*.*|\s*(?<!".*)\/\/\s*.*/g, "")
+        .replace(/(?<!(('|"|`).*))\/\*(.*)\*\//g, "")
+        .replace(/\/\*(\s*\n.*[^\/\*])+(\*\/)$/gm, "")
+        .replace(/(\/\*\*|\/\*)(\s*|.*\n\s*\*.*)*(\/)$/gm, ""));
+    })
+    .then(() => formatFile(editor));
+};
+
+
+export const isSupportLanguageCheck = (editor: vscode.TextEditor): boolean => {
+  const language = editor.document.languageId.toString();
+
+  if (language) {
+    return supportLanguage.includes(language);
+  }
+
+  return false;
+};
