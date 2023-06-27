680b9893167e39e2684cdbb2addd64aa1cdf2112
shizheng <ZhengShiWork@outlook.com>
2023-06-27 11:25:59 +0800

    remove all-comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,13 @@
 import * as vscode from "vscode";
+import { handler } from "./removeAllComments";
 
 export function activate(context: vscode.ExtensionContext) {
 
-  const disposable = vscode.commands.registerCommand("remove-all-comments-javascript.removeAllComments", () => {
-    vscode.window.showInformationMessage("Hello World from remove-all-comments-jsOrTs!");
+  const disposable = vscode.commands.registerTextEditorCommand("remove-all-comments-javascript.removeAllComments", (textEditor:vscode.TextEditor, edit: vscode.TextEditorEdit) => {
+    handler(textEditor, edit).removeComment(); 
   });
+
+  vscode.window.showInformationMessage("Remove successfully!");
 
   context.subscriptions.push(disposable);
 }
-
-export function deactivate() {}
---
+++ src/removeAllComments.ts
@@ -0,0 +1,64 @@
+import vscode = require("vscode");
+
+export function handler(editor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
+  const { document } = editor;
+
+  function getFileInfo() {
+    const selections = [
+      new vscode.Selection(new vscode.Position(0, 0), document.positionAt(document.getText().length)),
+    ];
+    const selection = selections[0];
+    const startLine = selection.start.line;
+    const endLine = selection.end.line;
+
+    return {
+      selection,
+      startLine,
+      endLine
+    };
+  }
+
+  function removeComment() {
+    const { selection, startLine, endLine } = getFileInfo();
+
+    editor
+      .edit((edit) => {
+        edit.replace(new vscode.Range(new vscode.Position(startLine, 0), new vscode.Position(endLine, selection.end.character)), document
+          .getText()
+          .replace(/\/\*(?<=\/\*)[^\/\*]{0,}?(?=\*\/)\*\//g, "")
+        // .replace(/\/\*(.*[^\/\*]\s?\n+)+(.*\*\/)$/gm, "")
+          .replace(/(?<=.*".*".*)\s*\/\/\s+.*|\s*(?<!".*)\/\/\s+.*/g, "")
+          .replace(/(?<!(('|"|`).*))\/\*(.*)\*\//g, "")
+          .replace(/\/\*(\s*\n.*[^\/\*])+(\*\/)$/gm, "")
+          .replace(/(\/\*\*|\/\*)(\s*|.*\n\s*\*.*)*(\/)$/gm, ""));
+      })
+      .then(formatFile);
+  }
+
+  function formatFile() {
+    const { endLine } = getFileInfo();
+
+    editor.edit((edit) => {
+      for (let line = 0; line < endLine; line++) {
+        const currentLineText = document.lineAt(line);
+
+        if (currentLineText.isEmptyOrWhitespace) {
+          edit.delete(new vscode.Selection(new vscode.Position(line, 0), new vscode.Position(line + 1, 0)));
+        }
+      }
+    });
+  }
+
+  const supportLanguage = [
+    "javascript",
+    "typescript",
+    "javascriptreact",
+    "typescriptreact",
+  ];
+  const language = editor.document.languageId.toString();
+
+  return {
+    isSupportLanguage: !!supportLanguage.find((_supportLanguage) => _supportLanguage === language),
+    removeComment,
+  };
+}
